e0ed514d79122a4ba5c7d849bde7d4126e148280
MichaelXt <miklmailplus@gmail.com>
2025-02-16 18:44:31 -0800

    building_size to tscn from tres

--- Assets/World/Buildings/Building2D.gd
+++ Assets/World/Buildings/Building2D.gd
@@ -7,6 +7,7 @@ class_name Building2D
 ## Data resource for the building
 @export var building_data: BuildingData
 @export var info_tab_widget: PackedScene
+@export var building_size: Vector2i = Vector2i(1, 1)
 
 ## To be overridden for functionality on inputs when the building is selected
 func handle_context_input(_event: InputEvent):
