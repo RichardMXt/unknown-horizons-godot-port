f09fabd30256c4106a82228ca45e44d872cb30f9
MichaelXt <miklmailplus@gmail.com>
2025-10-07 08:16:33 +0700

    units

--- Assets/World/Components/BaseComponent.gd
+++ Assets/World/Components/BaseComponent.gd
@@ -6,6 +6,8 @@ class_name BaseComponent
 
 signal unpaused
 
+@export var type: String
+
 func _ready() -> void:
   var child_components: Array[BaseComponent] = []
   for component in child_components:
