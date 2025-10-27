21af2ddce7b5306529fbad6e2f0735a6b2e263b6
MichaelXt <miklmailplus@gmail.com>
2025-10-27 10:33:03 +0700

    happiness and residents count for residences

--- Assets/UI/TabWidgets/SettlerOverviewTab.gd
+++ Assets/UI/TabWidgets/SettlerOverviewTab.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 @onready var caption_block:CaptionBlock = $CaptionBlock
 @onready var taxes_control: TaxesControl = %TaxesControl
+@onready var happiness_slider: HSlider = %HappinessSlider
+@onready var residents_count_label: Label = %ResidentsCountLabel
 
 var selected_node: WorldThing2D = null:
   set(value):
@@ -9,10 +11,14 @@ var selected_node: WorldThing2D = null:
     on_new_selected_node(value)
 
 func on_new_selected_node(node: WorldThing2D) -> void:
-  var building_selected: Building2D = node as Building2D
-  if building_selected != null:
-    self.taxes_control.tax_rate = GameStats.treasury.tax_rate_per_tier.get(building_selected.current_tier_val, 1.0)
-    self.taxes_control.tax_rate_changed.connect(func (tax_rate: float): GameStats.treasury.tax_rate_per_tier[building_selected.current_tier_val] = tax_rate)  
+  var residential: Residential = node as Residential
+  if residential != null:
+    self.taxes_control.tax_rate = GameStats.treasury.tax_rate_per_tier.get(residential.current_tier_val, 1.0)
+    self.taxes_control.tax_rate_changed.connect(func (tax_rate: float): GameStats.treasury.tax_rate_per_tier[residential.current_tier_val] = tax_rate)
+
+    self.happiness_slider.value = residential.get_happiness()
+
+    self.residents_count_label.text = str(residential.residence) + " / " + str(residential.max_residents_for_current_tier)
 
   for child in self.get_children():
     if "selected_node" in child:
