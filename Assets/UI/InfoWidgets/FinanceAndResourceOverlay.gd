d04bb343e87c15dec704bfe4e8d7ec239383df5b
Richie <richmailplus@gmail.com>
2025-11-08 17:12:00 +0700

    update cost display on each building being built change

--- Assets/UI/InfoWidgets/FinanceAndResourceOverlay.gd
+++ Assets/UI/InfoWidgets/FinanceAndResourceOverlay.gd
@@ -5,14 +5,24 @@ class_name FinanceAndResourceOverlay
 @onready var balance_info_button: BalanceInfoButton = %BalanceInfoButton
 @onready var resources_overlay: ResourceOverlay = %ResourcesOverlay
 
+var building_context: BuildingContext = null
+
 func on_context_changed(context: BaseContext) -> void:
-  var building: StringName = &""
-  var road_building_context: BuildingRoadContext = context as BuildingRoadContext
-  var building_context: BuildingContext = context as BuildingContext
+  var building := &""
+  var road_building_context := context as BuildingRoadContext
+  var new_context_as_building_context := context as BuildingContext
+  if self.building_context == null: # remember the building context
+    self.building_context = new_context_as_building_context
+
   if road_building_context != null:
     building = &"TRAIL" # trail toggled, set name as trail
-  if building_context != null:
-    building = building_context.building_to_build
+
+  if new_context_as_building_context != null:
+    if self.building_context.building_changed.is_connected(self.on_building_changed) == false:
+      self.building_context.building_changed.connect(self.on_building_changed)
+    building = self.building_context.building_to_build
+  elif self.building_context != null: # if the context is not the building context disconnect the update signal
+    self.building_context.building_changed.disconnect(self.on_building_changed)
 
   if building != &"":
     balance_info_button.show_building_cost_overlay(building)
@@ -21,3 +31,7 @@ func on_context_changed(context: BaseContext) -> void:
     balance_info_button.show_normal_overlay()
     resources_overlay.show_normal_overlay()
     return
+
+func on_building_changed(building: StringName) -> void:
+  self.balance_info_button.show_building_cost_overlay(building)
+  self.resources_overlay.show_building_cost_overlay(building)
